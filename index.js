af800f2ca8e74848c300e427a65de738667a28e2
Omar Nachar <omarnachar2@gmail.com>
2021-05-20 17:08:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-function shout(string) {
-  return string.toUpperCase()
-}
-
-function whisper(string) {
-  return string.toLowerCase()
-}
-
-function logShout(string) {
-  console.log(string.toUpperCase())
-}
-
-function logWhisper(string) {
-  console.log(string.toLowerCase())
-}
-
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-  }
-
-  if (string.toUpperCase() === string) {
-    return "YES INDEED!"
-  }
-
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
-
-  return "Are you eating enough?"
-}
+let divide = () => 2000/100
+let square = x => x * x;
+let add = (a, b) => a + b;
